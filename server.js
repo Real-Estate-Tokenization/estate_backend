da68551b304062c95c2444c06f88b8a7f39f9457
Sanket <sanketsharma241@gmail.com>
2025-03-23 17:42:24 +0530

    Update CORS configuration to include all estate1729.loca.lt domains

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: ['http://localhost:3000', 'http://127.0.0.1:3000', 'https://estate1729.loca.lt:3000'],
+  origin: ['http://localhost:3000', 'http://127.0.0.1:3000', 'https://estate1729.loca.lt', 'https://estate1729.loca.lt:3000', 'estate1729.loca.lt'],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
